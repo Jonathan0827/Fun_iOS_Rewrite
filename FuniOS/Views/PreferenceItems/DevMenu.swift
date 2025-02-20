fa8dfb12beb5bfde8cc54838f701b628acecf9fe
Jonathan0827 <limjunehyeop@gmail.com>
2025-02-20 20:09:52 +0900

    I don't remember what I did

---
+++ FuniOS/Views/PreferenceItems/DevMenu.swift
@@ -0,0 +1,18 @@
+//
+//  DevMenu.swift
+//  FuniOS
+//
+//  Created by 임준협 on 8/5/24.
+//
+
+import SwiftUI
+
+struct DevMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DevMenu()
+}
